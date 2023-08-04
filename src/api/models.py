9faab1cdda44958f3acbac1bf04d2aebc6e65025
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-08-04 11:31:28 +0000

    endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,110 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Planet(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    name = db.Column(db.String(250), nullable=True)
+    climate = db.Column(db.String(250), nullable=True)
+    population = db.Column(db.Integer, nullable=True)
+    orbital_period  = db.Column(db.Integer, nullable=True)
+    rotation_period = db.Column(db.Integer, nullable=True)
+    diameter = db.Column(db.Integer, nullable=True)
+    # favoritos = db.relationship ('favoritos', backref= 'planets', lazy=True)
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<planets %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name" : self.name,
+            "climate": self.climate,
+            "population": self.population, 
+            "orbital_period": self.orbital_period,  
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter
+            # do not serialize the password, its a security breach
+        }
+
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    birth_year = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.String(250), nullable=True)
+    height = db.Column(db.Integer, nullable=True)
+    skin_color = db.Column(db.String(250), nullable=True)
+    eye_color = db.Column(db.String(250), nullable=True)
+    # favoritos = db.relationship ('favoritos', backref= 'characters', lazy=True)
+    def __repr__(self):
+        return '<characters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender, 
+            "height": self.height,  
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorito(db.Model):
+    __tablename__ = 'favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    characters_id= db.Column(db.Integer, db.ForeignKey('characters.id'),nullable=True)
+    planets_id= db.Column(db.Integer, db.ForeignKey('planets.id'),nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                        nullable=False)
+    def __repr__(self):
+        return '<favoritos %r>' % self.id
+
+    def serialize(self):
+        character_query= Character.query.filter_by(id= self.characters_id).first()
+
+        if character_query is None:
+          character_resultado=  None
+        else:
+          character_resultado= character_query.serialize()['name']
+
+        planet_query= Planet.query.filter_by(id= self.planets_id).first()
+
+        if planet_query is None:
+          planet_resultado=  None
+        else:
+          planet_resultado= planet_query.serialize()['name']
+        
+
+        return {
+            "id": self.id,
+            "characters": character_resultado,
+            "planets": planet_resultado,
+            "user_id": self.user_id
+            # do not serialize the password, its a security breach // planet_query.serialize()['name'] // character_query.serialize()['name']
+        }
+    
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # favoritos = db.relationship ('favoritos', backref= 'users', lazy=True)
+
+
+    def __repr__(self):
+        return '<users %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import re
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,79 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# create one user
+@api.route('/users', methods=['POST'])
+def create_user():
+
+    request_body = request.get_json(force=True)
+
+    user = User(email=request_body['email'],
+                password=request_body['password'],
+                is_active=request_body['is_active'])
+    
+    if request_body['email'] is None or request_body['password'] is None or request_body['is_active'] is None:
+        return jsonify ({
+            'msg':'missing parameters (email, password, is_active are required)'
+        }), 400
+
+    # Verificamos email válido (pro)
+
+    def validar_email(email):
+    # Patrón de expresión regular para validar el email
+        patron_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        
+        # Usamos re.match() para verificar el patrón en el email proporcionado
+        if re.match(patron_email, email):
+            return True
+        else:
+            return False
+
+    if validar_email(request_body['email']):
+        print("El email es válido.")
+    else:
+        return jsonify ({
+            'msg':'wrong email format(check @ .)'
+        }), 400
+
+    db.session.add(user)
+    db.session.commit()
+
+    response_body = {
+       "results": 'User Created'
+    }
+
+    return jsonify(response_body), 200
+
+
+#login users
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"msg": "User dont exist"}), 404
+
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+#ruta protegida
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    if user is None:
+        return jsonify({"msg": "user do not exist"}), 404
+    return jsonify(user.serialize()), 200
